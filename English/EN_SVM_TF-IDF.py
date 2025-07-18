dfd60536e63108438d35509675ea4675112dff7e
dianam55 <dianamuresan55@yahoo.com>
2025-07-18 08:44:29 +0300

    Update EN_SVM_TF-IDF.py

--- English/EN_SVM_TF-IDF.py
+++ English/EN_SVM_TF-IDF.py
@@ -27,13 +27,13 @@ print(df_Clickbait_and_NonClickbait['label'].value_counts())
 
 #Preprocessing
 df_Clickbait_and_NonClickbait['Headline'] = df_Clickbait_and_NonClickbait['Headline'].str.lower()
-df_Clickbait_and_NonClickbait['Headline'] = df_Clickbait_and_NonClickbait['Headline'].apply(lambda x: re.sub(r"[^a-zA-Z\s.,!?;:]", '', x))
+df_Clickbait_and_NonClickbait['Headline'] = df_Clickbait_and_NonClickbait['Headline'].apply(lambda x: re.sub(r"[^a-zA-Z\s.,!?;:]", '', x)) #keeps letters, spaces, punctuation
 stop_words = set(stopwords.words('english'))
 negations = {'no', 'not', 'none', 'never', "n't"}  
 stop_words = stop_words.difference(negations)  #keep negation
 lemmatizer = WordNetLemmatizer()
 df_Clickbait_and_NonClickbait['tokens'] = df_Clickbait_and_NonClickbait['Headline'].apply(word_tokenize)
-df_Clickbait_and_NonClickbait['tokens'] = df_Clickbait_and_NonClickbait['tokens'].apply(lambda x: [word for word in x if word not in stop_words])
+df_Clickbait_and_NonClickbait['tokens'] = df_Clickbait_and_NonClickbait['tokens'].apply(lambda x: [word for word in x if word not in stop_words]) #removes stop words
 df_Clickbait_and_NonClickbait['tokens'] = df_Clickbait_and_NonClickbait['tokens'].apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
 df_Clickbait_and_NonClickbait['cleaned_Headline'] = df_Clickbait_and_NonClickbait['tokens'].apply(lambda x: ' '.join(x))
 
@@ -42,18 +42,18 @@ y = df_Clickbait_and_NonClickbait['label']
 X_train_text, X_test_text, y_train, y_test = train_test_split(X_text, y, test_size=0.2, random_state=42, stratify=y)
 
 #TF-IDF 
-tfidf_vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1, 2))  
+tfidf_vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1, 2))  #both unigrams and bigrams
 X_train = tfidf_vectorizer.fit_transform(X_train_text)
 X_test = tfidf_vectorizer.transform(X_test_text)
 
 param_grid = {
-    'C': [0.1, 1.0, 10.0],
+    'C': [0.1, 1.0, 10.0], #regularization strength
     'kernel': ['rbf', 'linear'],
     'gamma': ['scale', 'auto']
 }
 
 svm_model = SVC(random_state=42, class_weight='balanced')
-grid_search = GridSearchCV(svm_model, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+grid_search = GridSearchCV(svm_model, param_grid, cv=5, scoring='accuracy', n_jobs=-1) ##n_jobs=-1 use all available CPU cores
 grid_search.fit(X_train, y_train)
 
 #Best model
